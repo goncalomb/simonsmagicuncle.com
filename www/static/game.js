b864e0991145ec8d6b80e15fefca37b0d1eaff27
Gonçalo Baltazar <me@goncalomb.com>
2018-05-03 23:12:11 +0100

    Add basic game mechanics

--- www/static/game.js
+++ www/static/game.js
@@ -2,13 +2,39 @@
 
     var Game = window.Game = function(canvas) {
         this._te = new TEngine(canvas);
+        // canvas.style.cursor = 'none';
         if (window.location.hash == '#no-loader') {
+            this._registerEventListeners(canvas);
             this.start();
         } else {
             this._te.startLoaderEffect('uncle', () => {
+                this._registerEventListeners(canvas);
                 this.start();
             });
         }
+    }
+
+    Game.AREA_MIN_X = 23;
+    Game.AREA_MIN_Y = 5;
+
+    Game.prototype._registerEventListeners = function(canvas) {
+        var bound = canvas.getBoundingClientRect();
+        var addEv = function(el, ev, fn) {
+            ev.forEach((v) => { el.addEventListener(v, fn); });
+        }
+        addEv(canvas, ['mousemove', 'touchmove'], (e) => {
+            var p = e;
+            if (e.type == 'touchmove') {
+                p = e.targetTouches[0];
+            }
+            this._drawPointer(Math.floor(80*(p.clientX - bound.left)/canvas.width), Math.floor(30*(p.clientY - bound.top)/canvas.height));
+        });
+        addEv(canvas, ['mousedown', 'touchstart'], (e) => {
+            this._firing = true;
+        });
+        addEv(document, ['mouseup', 'touchend'], (e) => {
+            this._firing = false;
+        });
     }
 
     Game.prototype._drawPixelArt = function(data, x, y) {
@@ -70,13 +96,159 @@ ___# # #   # #__# #  #   ___#   #   # #  # #__# # #___   #__# #  # #___ #__ #___
         this._te.drawText('.', x + 7, y + 7);
     }
 
+    Game.prototype._drawPointer = function(x, y) {
+        if (x >= Game.AREA_MIN_X && y >= Game.AREA_MIN_Y) {
+            this._te.setColors(TEngine.COLOR_WHITE, TEngine.COLOR_BLACK);
+            this._te.clearRectangle(this._pointerX, this._pointerY, 1, 1);
+            this._te.drawText('\xc5', x, y);
+            this._pointerX = x;
+            this._pointerY = y;
+        }
+    }
+
+    Game.prototype._step = function() {
+        // update eyes
+        if (this._firing) {
+            this._te.setColors(TEngine.COLOR_BRIGHT_RED, TEngine.COLOR_BRIGHT_MAGENTA);
+        } else {
+            this._te.setColors(TEngine.COLOR_BLACK, TEngine.COLOR_BRIGHT_MAGENTA);
+        }
+        this._te.drawText('\x07', 10 + 5, 9 + 2);
+        this._te.drawText('\x07', 10 + 7, 9 + 2);
+
+        // update lasers
+        this._te.setColors(TEngine.COLOR_BRIGHT_RED, TEngine.COLOR_BLACK);
+        if (this._firing) {
+            this._lasers.push(new Game.Laser(this));
+        }
+        for (var i = 0, l = this._lasers.length; i < l; i++) {
+            if (this._lasers[i].step()) {
+                this._lasers.splice(i--, 1);
+                l--;
+            }
+        }
+        // update arrows
+        this._te.setColors(TEngine.COLOR_BRIGHT_WHITE, TEngine.COLOR_BLACK);
+        if (Math.random() < 0.40) {
+            this._arrows.push(new Game.Arrow(this));
+        }
+        for (var i = 0, l = this._arrows.length; i < l; i++) {
+            if (this._arrows[i].step()) {
+                this._arrows.splice(i--, 1);
+                l--;
+            }
+        }
+    }
+
     Game.prototype.start = function() {
+        this._pointerX = 79;
+        this._pointerY = 29;
+        this._firing = false;
+        this._lasers = [];
+        this._arrows = [];
         this._drawLogo();
         this._drawUncle(10, 9);
+        this._interval = setInterval(() => {
+            this._step();
+        }, 50);
     }
 
     Game.prototype.stop = function() {
+        clearInterval(this._interval);
+    }
 
+    // Game.Arrow
+
+    Game.Arrow = function(game) {
+        this._game = game;
+        this._x = 79;
+        this._y = Game.Arrow.MIN_Y + Math.floor((Game.Arrow.MAX_Y - Game.Arrow.MIN_Y)*Math.random());
+        this._xf = this._x;
+    }
+
+    Game.Arrow.MIN_Y = 9;
+    Game.Arrow.MAX_Y = 26;
+
+    Game.Arrow.prototype.step = function() {
+        // clear
+        this._game._te.clearRectangle(this._x, this._y, 5, 1);
+        // update
+        this._xf -= 0.5;
+        this._x = Math.floor(this._xf);
+        // test boundary
+        if (this._x < 0) {
+            return true;
+        }
+        // draw
+        this._game._te.drawText('<--<', this._x, this._y);
+    }
+
+    Game.Arrow.prototype.destroy = function() {
+        this._game._te.clearRectangle(this._x, this._y, 5, 1);
+    }
+
+    // Game.Laser
+
+    Game.Laser = function(game) {
+        this._game = game;
+        this._x = Game.Laser.START_X;
+        this._y = Game.Laser.START_Y;
+        this._xf = this._x;
+        this._yf = this._y;
+
+        // calculate angle
+        var a = Math.atan2(this._game._pointerY - this._y, this._game._pointerX - this._x);
+        this._speedX = Math.cos(a);
+        this._speedY = Math.sin(a);
+        if (Math.abs(a) > Math.PI/4) {
+            this._c = '|';
+        } else if (a > Math.PI/16) {
+            this._c = '\\';
+        } else if (a < -Math.PI/16) {
+            this._c = '/';
+        } else{
+            this._c = '\x16';
+        }
+
+        this._game._te.drawText(this._c, this._x, this._y);
+    }
+
+    Game.Laser.START_X = 19;
+    Game.Laser.START_Y = 11;
+
+    Game.Laser.prototype.step = function() {
+        // test collision with arrow
+        var hit = false;
+        for (var i = 0, l = this._game._arrows.length; i < l; i++) {
+            var arr = this._game._arrows[i];
+            if ((this._x == arr._x || this._x == arr._x + 1 ) && this._y == arr._y) {
+                this.destroy();
+                arr.destroy();
+                this._game._arrows.splice(i--, 1);
+                l--;
+                hit = true;
+            }
+        }
+        if (hit) {
+            return true;
+        }
+        // clear
+        this._game._te.clearRectangle(this._x, this._y, 1, 1);
+        // update
+        this._xf += this._speedX;
+        this._yf += this._speedY;
+        this._x = Math.round(this._xf);
+        this._y = Math.round(this._yf);
+        // test boundary
+        if (this._x < Game.START_X || this._y < Game.AREA_MIN_Y || this._x > 79 || this._y > 29) {
+            return true;
+        }
+        // draw
+        this._game._te.drawText(this._c, this._x, this._y);
+    }
+
+    Game.Laser.prototype.destroy = function() {
+        this._game._te.clearRectangle(this._x, this._y, 1, 1);
     }
 
 })();
